5c39ace3c9f9ee05e61236085fb5c292b00d6866
ebmoynahan <44822751+ebmoynahan@users.noreply.github.com>
2019-02-07 18:28:24 -0500

    Update traintimes.js

--- traintimes.js
+++ traintimes.js
@@ -1,20 +1,18 @@
 var config = {
-    apiKey: "AIzaSyDkFPXt4XkPr_4Vyu5PmGLepXgOXZHjUCw",
-    authDomain: "train-times-homework-605c6.firebaseapp.com",
-    databaseURL: "https://train-times-homework-605c6.firebaseio.com",
-    projectId: "train-times-homework-605c6",
-    storageBucket: "train-times-homework-605c6.appspot.com",
-    messagingSenderId: "501343635743"
-  };
-  firebase.initializeApp(config);
-
-
+  apiKey: "AIzaSyDkFPXt4XkPr_4Vyu5PmGLepXgOXZHjUCw",
+  authDomain: "train-times-homework-605c6.firebaseapp.com",
+  databaseURL: "https://train-times-homework-605c6.firebaseio.com",
+  projectId: "train-times-homework-605c6",
+  storageBucket: "train-times-homework-605c6.appspot.com",
+  messagingSenderId: "501343635743"
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
   $('#userSubmitButton').on("click", function() {
     var trainName = $("#trainNameInput").val().trim()
     var destination = $("#destinationInput").val().trim();
-    var firstTrainTime = moment($("#timeInput").val().trim(), "HH:mm").format("HH:mm")
+    var firstTrainTime = moment($("#firstTrainNameInput").val().trim(), "HH:mm").format("HH:mm")
     var frequency = $("#frequencyInput").val().trim()
     var userTrain = {
         name: trainName,
@@ -26,7 +24,7 @@ var database = firebase.database();
     console.log(userTrain.name)
     $("#trainNameInput").val("")
     $("#destinationInput").val("")
-    $("#timeInput").val("")
+    $("#firstTrainNameInput").val("")
     $("#frequencyInput").val("")
     return false
   });
@@ -35,7 +33,7 @@ var database = firebase.database();
    
     var trainName = childSnapshot.val().name;
     var destination = childSnapshot.val().userDestination;
-    var firstTrain = childSnapshot.val().firstTrain;
+    var firstTrainTime = childSnapshot.val().firstTrainTime;
     var frequency = childSnapshot.val().trainFrequency;
 
     var timeConversion = moment(firstTrainTime, "HH:mm");
@@ -45,7 +43,7 @@ var database = firebase.database();
     console.log(currentTime)
 
     var timeDifference = moment().diff(moment(timeConversion), "minutes");
-    console.log(firstTrain)
+    console.log(firstTrainTime)
     console.log(timeDifference)
 
     var timeRemaining = timeDifference % frequency;
